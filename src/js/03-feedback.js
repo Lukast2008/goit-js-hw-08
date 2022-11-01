3d02f15dafa04882fe221736710de24e84273f92
Lukast2008 <lukast2008@gmail.com>
2022-11-01 18:58:07 +0200

    over done HW8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,13 +1,45 @@
+'use strict';
 import throttle from 'lodash.throttle';
+import localstorageApi from './localstorage';
 
 const getEmailValue = document.querySelector('.feedback-form');
 
-const getMassageValue = document.querySelector('message');
+const CONTACT_FORM_KEY = 'feedback-form-state';
 
-const handleButtonSub = document.querySelector('button');
+const fillContactFild = form => {
+  const { elements: contactFormEl } = form;
 
-getEmailValue.addEventListener('submit', event => {
+  const formData = localstorageApi.load(CONTACT_FORM_KEY);
+
+  if (!formData) {
+    return;
+  }
+
+  const keys = Object.keys(formData);
+
+  for (const key of keys) {
+    contactFormEl[key].value = formData[key];
+  }
+};
+
+const handeleEmeilForm = throttle(({ target }) => {
+  const contactFormElementName = target.name;
+  const contactFormElementValue = target.value;
+
+  const formData = localstorageApi.load(CONTACT_FORM_KEY) || {};
+
+  formData[contactFormElementName] = contactFormElementValue;
+  console.log(contactFormElementName, contactFormElementValue);
+  localstorageApi.save(CONTACT_FORM_KEY, formData);
+}, 500);
+
+const hendlContactFormSubmit = event => {
   event.preventDefault();
-  let eventValue = event.target.email.value;
-  console.log(eventValue);
-});
+  event.target.reset();
+  localstorageApi.remove(CONTACT_FORM_KEY);
+};
+
+getEmailValue.addEventListener('input', handeleEmeilForm);
+getEmailValue.addEventListener('submit', hendlContactFormSubmit);
+
+fillContactFild(getEmailValue);
---
+++ src/js/localstorage.js
@@ -0,0 +1,27 @@
+const save = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error('set state error: ', error.message);
+  }
+};
+
+const load = key => {
+  try {
+    serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('get state error: ', error.message);
+  }
+};
+
+const remove = key => {
+  try {
+    localStorage.removeItem(key);
+  } catch (error) {
+    console.error('Remove item error: ', error.message);
+  }
+};
+
+export default { save, load, remove };
