75f76e9d74ff22b2648fe3285632e3748fd46d3b
Lukast2008 <lukast2008@gmail.com>
2022-11-01 14:14:16 +0200

    e

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,13 @@
+import throttle from 'lodash.throttle';
 
+const getEmailValue = document.querySelector('.feedback-form');
+
+const getMassageValue = document.querySelector('message');
+
+const handleButtonSub = document.querySelector('button');
+
+getEmailValue.addEventListener('submit', event => {
+  event.preventDefault();
+  let eventValue = event.target.email.value;
+  console.log(eventValue);
+});
